00813272fa3745ef5f229ec790496d6e8129ca05
JaesungLeee <viki0114@naver.com>
2022-07-14 16:36:20 +0900

    [FEAT] #7 로그인 레이아웃 구현

---
+++ app/src/main/java/com/comjeong/nomadworker/ui/signin/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.comjeong.nomadworker.ui.signin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.comjeong.nomadworker.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
