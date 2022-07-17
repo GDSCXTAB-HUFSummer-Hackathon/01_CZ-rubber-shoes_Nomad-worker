3cd1d7dd6e8af7d450b65a63fe8722805b657e50
Yoon-Min <yoonmin0113@naver.com>
2022-07-18 04:51:40 +0900

    [FEAT] #16 피드 페이지, 새로운 피드 작성 페이지 레이아웃 구현

---
+++ app/src/main/java/com/comjeong/nomadworker/ui/feed/NewFeedContentChoiceFragment.kt
@@ -0,0 +1,16 @@
+package com.comjeong.nomadworker.ui.feed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.comjeong.nomadworker.R
+import com.comjeong.nomadworker.databinding.FragmentNewFeedContentChoiceBinding
+import com.comjeong.nomadworker.ui.common.BaseFragment
+
+class NewFeedContentChoiceFragment : BaseFragment<FragmentNewFeedContentChoiceBinding>(R.layout.fragment_new_feed_content_choice) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
