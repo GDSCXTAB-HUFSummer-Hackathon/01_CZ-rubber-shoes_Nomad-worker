db2e6dfb7e2fe8e77597004afecc29d5481f8f1d
Yoon-Min <yoonmin0113@naver.com>
2022-07-17 20:18:59 +0900

    [FEAT] #12 마이페이지 설정 레이아웃 구현

--- app/src/main/java/com/comjeong/nomadworker/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/comjeong/nomadworker/ui/mypage/MyPageFragment.kt
@@ -2,12 +2,25 @@ package com.comjeong.nomadworker.ui.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.FragmentManager
 import com.comjeong.nomadworker.R
 import com.comjeong.nomadworker.databinding.FragmentMyPageBinding
 import com.comjeong.nomadworker.ui.common.BaseFragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnMypageSetting.setOnClickListener {
+            showBottomSheetDialog()
+        }
     }
+
+    private fun showBottomSheetDialog(){
+        val bottomSheet = BottomSheetDialog(requireContext(), R.style.CustomBottomSheetDialog)
+        bottomSheet.setContentView(R.layout.fragment_my_page_setting_bottom_sheet)
+        bottomSheet.show()
+    }
+
 }
